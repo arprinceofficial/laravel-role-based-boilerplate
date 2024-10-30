93127760864b801d5ab16c3f75863ac50ea35bcd
arprinceofficial <ashiqurr04@gmail.com>
2024-10-31 01:00:09 +0600

    fix Laravel Sanctum: Route [login] not defined
    1. set header for every request 'Accept', 'application/json' => app\Http\Middleware\ForceJsonResponse.php
    2. register the middleware => ForceJsonResponse
    3. remove unnecessary code and update code in route/api

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/test', function (Request $request){
-    return response()->json([
-        'message' => 'Hello World'
-    ]);
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/current-user', [AuthController::class, 'currentUser'])->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/current-user', [AuthController::class, 'currentUser']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
